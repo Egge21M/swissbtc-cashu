c97b18b863c29f57c6c42bc4d319b436f53bcb61
Egge <egge@my2sats.space>
2025-04-26 08:46:52 +0200

    setup

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,37 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+declare global {
+  interface Window {
+    cashu: any;
+  }
+}
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const mintUrl = "";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+class Cashu {
+  constructor(private mintUrl: string) {}
+
+  async getInfo() {
+    // Add code
+  }
+
+  async getMintQuote() {
+    // Add code
+  }
+
+  async mintProofs(quoteId: string) {
+    // Add code
+  }
+
+  async encodeToken() {
+    // Add code
+  }
+
+  async sendProofs() {
+    // Add code
+  }
+
+  async receiveProofs() {
+    // Add code
+  }
+}
+
+window.cashu = new Cashu(mintUrl);
